c744bad7c31f6a760158b1933cd6c6b0a22dd4d3
yj <1324028467@qq.com>
2023-01-15 16:14:37 +0800

    我我

--- api-service/app/Modules/Admin/Logic/LoginLogic.php
+++ api-service/app/Modules/Admin/Logic/LoginLogic.php
@@ -6,9 +6,14 @@
 
 namespace App\Modules\Admin\Logic;
 
+<<<<<<< HEAD
 
 use App\Modules\Admin\Library\Login\LoginCode;
 use App\Modules\Admin\Library\LoginLibrary;
+=======
+use App\Library\Login\LoginCode;
+use App\Library\Login\LoginLibrary;
+>>>>>>> 1ff3589 (我我)
 
 class LoginLogic
 {
@@ -16,7 +21,11 @@ class LoginLogic
         $loginLibrary = LoginLibrary::init($username,$password,$type);
         $loginLibrary =  $loginLibrary->check();
         if($loginLibrary->code!==LoginCode::SUCCESS){
+<<<<<<< HEAD
             Error($loginLibrary->code);
+=======
+            _Error((LoginCode::getMessage($loginLibrary->code)));
+>>>>>>> 1ff3589 (我我)
         }
         return $loginLibrary->loginSuccess();
     }
